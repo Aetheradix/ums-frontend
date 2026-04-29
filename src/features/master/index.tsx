a580d759009b111cc771e686b995267917dfa1d2
sayedahmedali <ahmedalisfa.it@gmail.com>
2026-04-29 15:57:20 +0530

    [Department] - feature : added fculty department master in master index file and mainLayout.

--- src/features/master/index.tsx
+++ src/features/master/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import SubjectCategory from './course/subject-category';
 import OfficeType from './faculty/office-type';
+import Department from './faculty/department';
 import Location from './location';
 
 export default function Master() {
@@ -9,6 +10,7 @@ export default function Master() {
       <Route path="location/*" element={<Location />} />
       <Route path="subject-category/*" element={<SubjectCategory />} />
       <Route path="office-type/*" element={<OfficeType />} />
+      <Route path="department/*" element={<Department />} />
     </Routes>
   );
 }
