70d0893475c37cf51d8d0d5d79824b740ce20b84
Naveen <naveensen8899@gmail.com>
2026-05-11 15:55:41 +0530

    [Grant-Master]-Feature : Remove Sift isActiv column to the type script

--- src/types/master/grant/grant.d.ts
+++ src/types/master/grant/grant.d.ts
@@ -2,15 +2,17 @@ declare namespace Master {
   namespace Grant {
     interface GrantTypeForm {
       name: string;
+      isActive: boolean;
     }
 
-    type GrantTypeItem = Data.WithId<GrantTypeForm & { isActive: boolean }>;
+    type GrantTypeItem = Data.WithId<GrantTypeForm>;
 
     interface GrantCategoryForm {
       name: string;
       grantTypeId: number;
+      isActive: boolean;
     }
 
-    type GrantCategoryItem = Data.WithId<GrantCategoryForm & { isActive: boolean }>;
+    type GrantCategoryItem = Data.WithId<GrantCategoryForm>;
   }
 }
