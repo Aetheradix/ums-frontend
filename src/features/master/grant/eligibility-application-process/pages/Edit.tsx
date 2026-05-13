4651d2c663d3a22bc68290b08ac7a7d3b80cdb37
SadhanaGupta2011 <Guptasadhana207@gmail.com>
2026-05-13 18:58:13 +0530

    Change in edit files according to param.tsx

--- src/features/master/grant/eligibility-application-process/pages/Edit.tsx
+++ src/features/master/grant/eligibility-application-process/pages/Edit.tsx
@@ -1,12 +1,15 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import { ToastService } from 'services';
-import { FormCard, FormPage } from 'shared/new-components';
 import { Loader } from 'shared/components/progress';
 import { useParamsId } from 'shared/hooks/params';
+import { FormCard, FormPage } from 'shared/new-components';
 import { masterUrls } from '../../../urls';
 import EligibilityApplicationProcessForm from '../components/EligibilityApplicationProcessForm';
-import { useEligibilityApplicationProcessQuery, useUpdateEligibilityApplicationProcessMutation } from '../queries';
+import {
+  useEligibilityApplicationProcessQuery,
+  useUpdateEligibilityApplicationProcessMutation,
+} from '../queries';
 
 const DEFAULT = {
   grantTypeId: 0,
@@ -18,9 +21,11 @@ const DEFAULT = {
 
 export default function Edit() {
   const navigate = useNavigate();
-  const id = useParamsId();
-  const { mutateAsync, isPending } = useUpdateEligibilityApplicationProcessMutation(id);
-  const { data = DEFAULT, isLoading } = useEligibilityApplicationProcessQuery(id);
+  const id = Number(useParamsId());
+  const { mutateAsync, isPending } =
+    useUpdateEligibilityApplicationProcessMutation(id);
+  const { data = DEFAULT, isLoading } =
+    useEligibilityApplicationProcessQuery(id);
 
   const handleBack = useCallback(() => {
     navigate(masterUrls.eligibilityApplicationProcess.root);
@@ -30,11 +35,15 @@ export default function Edit() {
     return <Loader />;
   }
 
-  async function handleSubmit(data: Master.Grant.EligibilityApplicationProcessForm) {
+  async function handleSubmit(
+    data: Master.Grant.EligibilityApplicationProcessForm
+  ) {
     try {
       const result = await mutateAsync(data);
       if (result) {
-        ToastService.success('Eligibility Application Process updated successfully.');
+        ToastService.success(
+          'Eligibility Application Process updated successfully.'
+        );
         handleBack();
       }
     } catch {
