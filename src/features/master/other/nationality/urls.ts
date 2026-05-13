d05f28223bf8f5817bf08269b92f943f492998ff
Ankit Sahu <sahuankit90980@gmail.com>
2026-05-13 18:16:01 +0530

    [nationality-master] refactor: improve code quality and move files to appropriate folders

--- src/features/master/other/nationality/urls.ts
+++ src/features/master/other/nationality/urls.ts
@@ -2,7 +2,7 @@ export function nationalityUrls(baseUrl: string) {
   const url = `${baseUrl}/nationality`;
   return {
     root: url,
-    edit: (id: number) => `${url}/${id}/edit`,
+    edit: (id: number) => `${url}/edit/${id}`,
     create: `${url}/create`,
   };
 }
