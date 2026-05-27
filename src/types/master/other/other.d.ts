7b67255a92f68cac6c7f6e2c5805966578b54aeb
Rinku Sahu <rinkusahusfa@gmail.com>
2026-05-27 11:39:33 +0530

    [Master]-refactor: add proper interface to Appraisal Application status dictionary

--- src/types/master/other/other.d.ts
+++ src/types/master/other/other.d.ts
@@ -151,6 +151,9 @@ declare namespace Master {
       text: string;
     }
     interface AppraisalApplicationForwardedToTypeItem {
+      id: string;
+      text: string;
+    }
     interface PerformanceRatingItem {
       id: string;
       text: string;
