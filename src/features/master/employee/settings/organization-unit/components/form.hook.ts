473bbb8d45675cdc02bb3b69296183773efa419b
Rishikesh Moon <rishikeshmoon@gmail.com>
2026-06-24 17:13:18 +0530

    [Master] - refactor : refactor validations for employee-management master.

--- src/features/master/employee/settings/organization-unit/components/form.hook.ts
+++ src/features/master/employee/settings/organization-unit/components/form.hook.ts
@@ -1,8 +1,18 @@
+import { errors } from 'config/errors';
 import { useAppForm } from 'shared/hooks/form';
-import validation from 'shared/utils/validation';
+import validation, { expressions, keys } from 'shared/utils/validation';
 
 const schema = validation.create<Master.Employee.OrganizationUnitForm>(o => ({
-  name: o.string().required().max(150).label('Organization Unit Name'),
+  name: o
+    .string()
+    .required()
+    .pattern(expressions.englishOnly)
+    .messages({
+      [keys.string.pattern]: errors.englishOnly,
+    })
+    .min(2)
+    .max(150)
+    .label('Organization Unit Name'),
 }));
 
 export function useOrganizationUnitForm(
